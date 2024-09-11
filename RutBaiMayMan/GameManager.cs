62d1b9560ff3e78b770bce9c6c95a0c8c00e4b42
Batuhan <anhtubui574@gmail.com>
2024-09-11 10:00:22 +0700

    Add GameManager class to manage game logic in RutBaiMayMan
    Introduced a new internal GameManager class within the RutBaiMayMan namespace. This class includes methods to set up a game with two players, determine the winner based on scores, generate a list of 10 unique random cards, and convert card numbers to their integer values. The class ensures no duplicate cards are drawn using a Random instance.

---
+++ RutBaiMayMan/GameManager.cs
@@ -0,0 +1,89 @@
+﻿using RutBaiMayMan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RutBaiMayMan
+{
+
+    internal class GameManager
+    {
+        private Random _random = new Random();
+        public Game SetupGame(Player player1, Player player2)
+        {
+            var game = new Game
+            {
+                Player1 = player1,
+                Player2 = player2,
+                Cards = new List<Card>(),
+                Player1Score = 0,
+                Player2Score = 0,
+                PlayedAt = DateTime.Now
+            };
+
+            return game;
+        }
+
+        public GameResult DetermineWinner(Game game, int player1Score, int player2Score)
+        {
+            game.Player1Score = player1Score;
+            game.Player2Score = player2Score;
+            if (player1Score > player2Score)
+            {
+                return GameResult.Player1Win;
+            }
+            else if (player1Score < player2Score)
+            {
+                return GameResult.Player2Win;
+            }
+            else
+            {
+                return GameResult.Draw;
+            }
+        }
+        private List<Card> GetRandomCards()
+        {
+            string[] cardIcons = { "heart", "spade", "diamond", "club" };
+            string[] cardNumbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+            List<Card> cardsDrawn = new List<Card>();
+            HashSet<string> drawnCards = new HashSet<string>(); // track already drawn cards
+
+            while (cardsDrawn.Count < 10)
+            {
+                string cardIcon = cardIcons[_random.Next(cardIcons.Length)];
+                string cardNumber = cardNumbers[_random.Next(cardNumbers.Length)];
+                string card = $"{cardNumber} of {cardIcon}";
+
+                // Check if the card was already drawn
+                if (!drawnCards.Contains(card))
+                {
+                    drawnCards.Add(card);
+
+                    // Add to the list of drawn cards
+                    cardsDrawn.Add(new Card
+                    {
+                        Suit = cardIcon,
+                        Value = GetCardValue(cardNumber)
+                    });
+                }
+            }
+
+            return cardsDrawn;
+        }
+
+        private int GetCardValue(string cardNumber)
+        {
+            // Convert card number to its corresponding value for game logic
+            return cardNumber switch
+            {
+                "A" => 1,
+                "J" => 11,
+                "Q" => 12,
+                "K" => 13,
+                _ => int.Parse(cardNumber)
+            };
+        }
+    }
+}
